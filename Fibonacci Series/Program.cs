0ee64128a76a71253db9219da1c2b1171ac2112d
shubhampachghare <shubhampachghare@gmail.com>
2022-12-03 01:42:43 +0530

    [Shubham]- FibonacciSeries

--- Fibonacci Series/Program.cs
+++ Fibonacci Series/Program.cs
@@ -1,8 +1,13 @@
-﻿public class Program
+﻿using Fibonacci_Series;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome logical Program");
+        FibonacciSeries obj = new FibonacciSeries();
+        obj.FibonacciSeries1(); 
+
     }
 }
 
