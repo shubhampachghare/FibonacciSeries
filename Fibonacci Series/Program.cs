5084edae54be8a3cd7ddf4ab906a3a792a917214
shubhampachghare <shubhampachghare@gmail.com>
2022-12-03 13:41:59 +0530

    [Shubham]- PrimeNumber

--- Fibonacci Series/Program.cs
+++ Fibonacci Series/Program.cs
@@ -1,18 +1,21 @@
 ﻿using Fibonacci_Series;
 using PerfectNumber;
-
+using PrimeNumber;
 public class Program
 {
     public static void Main(string[] args)
     {
         /*Console.WriteLine("Welcome logical Program");
         FibonacciSeries obj = new FibonacciSeries();
-        obj.FibonacciSeries1();*/
+        obj.FibonacciSeries1();
         
 
         PerfectNumber1 obj1 = new PerfectNumber1();
         obj1.number();
+        */
 
+        PrimeNumber1 obj2 = new PrimeNumber1();
+        obj2.Prime();
     }
 }
 
