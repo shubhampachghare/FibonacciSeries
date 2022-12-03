51a3497c590a0d00645b961518673892d2e0ecbb
shubhampachghare <shubhampachghare@gmail.com>
2022-12-03 13:29:49 +0530

    [Shubham]- PerfectNUmber

--- Fibonacci Series/Program.cs
+++ Fibonacci Series/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Fibonacci_Series;
+using PerfectNumber;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome logical Program");
+        /*Console.WriteLine("Welcome logical Program");
         FibonacciSeries obj = new FibonacciSeries();
-        obj.FibonacciSeries1(); 
+        obj.FibonacciSeries1();*/
+        
+
+        PerfectNumber1 obj1 = new PerfectNumber1();
+        obj1.number();
 
     }
 }
